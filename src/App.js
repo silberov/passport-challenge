fc84fc1470ab83d1efabc6fdf5d5e85c53494736
Wilfredo <casas.farach@gmail.com>
2020-10-22 09:38:09 +0200

    class with instructions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// INSTRUCTIONS
+// Change this functional component to a class component
+// Typing in the inputs should show the right info in Kanye's passport
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input placeholder="First Name" />
+        <input placeholder="Last Name" />
+        <input placeholder="Nationality" />  
+        <input placeholder="Place of Birth" />
+      </form>
+      <div className="passport">
+        <h2>Kanye's Passport</h2>
+        <div className="flex">
+          <img
+            className="profile"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Kanye_West_at_the_2009_Tribeca_Film_Festival-2_%28cropped%29.jpg/1200px-Kanye_West_at_the_2009_Tribeca_Film_Festival-2_%28cropped%29.jpg"
+          />
+          <div className="results">
+            <div className="flex">
+              <p>First Name: </p>
+            </div>
+            <div className="flex">
+              <p>Last Name: </p>
+            </div>
+            <div className="flex">
+              <p>Nationality: </p>
+            </div>
+            <div className="flex">
+              <p>Place of Birth:</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
